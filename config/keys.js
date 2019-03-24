ffeb219b24980a6419804c90c72bf329431e5b4e
Kehinde Adelugba <kennyadenat09@gmail.com>
2019-03-24 18:24:28 +0100

    Second Update

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mongodb: {
+    dev: 'mongodb://localhost:27017/kate',
+    prod: ''
+  },
+  session: {
+    cookieKey: "icanhehtyurjnghnk575768nnfhndvj4iunfurnnbg"
+  },
+  secret: "edrxfctgvyhubijnko5467yuiordcfv45655fgdttejdorywudn756672mmsgyfus",
+  port: process.env.PORT || 3000
+}
